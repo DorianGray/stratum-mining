76608c2f05b4c245e1a825e849a12f2f31c5f1f7
Robert Andrew Ditthardt <drew.ditthardt@gmail.com>
2018-02-11 14:59:11 -0800

    virtualenv

--- virtualenv.sh
+++ virtualenv.sh
@@ -10,14 +10,18 @@ fi
 
 EZ="$PY -m easy_install"
 VENV="$PY -m virtualenv"
-$VENV 2>&1 > /dev/null
+$VENV 2>&1 > /dev/null || true
 if [ ! $? -eq 0 ]; then
+  echo "Installing missing virtualenv..."
   $EZ -U virtualenv
 fi
 
 $VENV --no-site-packages --python="$PY" .venv/
 PY=".venv/bin/python"
-
 PIP="$PY -m pip"
-$PIP install -e . -rrequirements.txt
-$PIP install -e externals/stratum -e externals/argon2d-hash
+
+$PIP install\
+  -e .\
+  -e externals/stratum\
+  -e externals/argon2d-hash\
+  -rrequirements.txt
